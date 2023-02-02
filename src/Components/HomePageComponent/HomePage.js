0c64fc3b0aef084688b3948ae995a1d55a586600
Kavish <kavishmathur11402@gmail.com>
2023-02-03 01:07:31 +0530

    things changed again

--- src/Components/HomePageComponent/HomePage.js
+++ src/Components/HomePageComponent/HomePage.js
@@ -1,16 +1,12 @@
 import TopBanner from "@/Components/HomePageComponent/TopBanner";
-import Top4LatestVideo from "@/Components/HomePageComponent/Top4LatestVideo";
 import WhyLCWD from "@/Components/HomePageComponent/WhyLCWD";
-import FreeCourses from "@/Components/HomePageComponent/FreeCourses";
-import YoutubeCount from "@/Components/HomePageComponent/YoutubeCount";
-import LCWDStrategy from "@/Components/HomePageComponent/LCWDStrategy";
 
 export default function HomePage()
 {
   return(
       <>
           <TopBanner/>
-          {/*<WhyLCWD/>
+          <WhyLCWD/>
           {/*<Top4LatestVideo/>
           {/*<YoutubeCount/>
           {/*<FreeCourses/>
--- src/Components/HomePageComponent/WhyLCWD.js
+++ src/Components/HomePageComponent/WhyLCWD.js
@@ -7,12 +7,14 @@ export default function WhyLCWD()
   return(
       <div className={"flex flex-col justify-center py-20"}>
 
-          <h1 className={"text-center text-5xl font-bold underline underline-offset-8 text-primary-dark mb-24"}> Why Learn Code With Durgesh ? </h1>
+          <h1 className={" text-center text-xl font-bold underline underline-offset-8 text-primary-dark mb-16"}> Why Learn Code With Durgesh ? </h1>
 
-          <div className={"w-fit grid grid-cols-3 gap-24 mx-auto"}>
+          <div className={"w-fit grid grid-cols-1 gap-12 mx-auto"}>
+
               <ExplanationCard icon={cardIcon} title={"Best Explanation"} description={"Each and every concepts are well explained so that you can learn effectively."}/>
               <ExplanationCard icon={cardIcon} title={"Best Explanation"} description={"Each and every concepts are well explained so that you can learn effectively."}/>
               <ExplanationCard icon={cardIcon} title={"Best Explanation"} description={"Each and every concepts are well explained so that you can learn effectively."}/>
+
           </div>
       </div>
   );
