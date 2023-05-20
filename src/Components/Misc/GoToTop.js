510b75563763a4886541b24950e9288173bb6042
Kavish <kavishmathur11402@gmail.com>
2023-05-20 18:01:19 +0530

    GoToTop.js BG Color Changed

--- src/Components/Misc/GoToTop.js
+++ src/Components/Misc/GoToTop.js
@@ -26,7 +26,7 @@ export default function GoToTop()
 
     return (
         ( isVisible ) &&
-        <div className={"fixed top-[90%] right-8 z-50 bg-primary-medium rounded-full overflow-clip p-3 cursor-pointer h-fit w-fit"} onClick={ ()=>{ goToBtn() } } >
+        <div className={"fixed top-[90%] right-8 z-50 bg-purple-700 rounded-full overflow-clip p-3 cursor-pointer h-fit w-fit"} onClick={ ()=>{ goToBtn() } } >
           <svg className={"w-6 h-6 md:w-10 md:h-10 text-white animate-bounce"}
                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" >
             <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
